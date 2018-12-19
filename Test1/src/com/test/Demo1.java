231be0f408baee69a8fa562a7534fd4cc1055850
rongyiliu <1843417581@qq.com>
2018-12-19 14:50:41 +0800

    刘奕荣新增了一个liuyirong方法

--- Test1/src/com/test/Demo1.java
+++ Test1/src/com/test/Demo1.java
@@ -1,11 +1,17 @@
 package com.test;
 
 public interface Demo1{
+	
    public void add();
+   
    public void del();
 
    public void update();
 
    public void select();
+   
    public int select1();
+   
+   public void liuyirong();
+   
 }
